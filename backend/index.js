7e0cef2af42a109beba833c7e3e2fe2b47f7b9c5
GaganjotHans <109852605+GaganjotHans@users.noreply.github.com>
2024-01-07 18:42:15 -0500

    Intial Commit

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const connectToMongo = require('./db');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Gaganjot Hans!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+connectToMongo();
+
+
